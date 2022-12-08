e100e77e9548e5611cb9dfb87fb4c8143bb5d62a
Maria Isabel Molina <101520961+isasoul@users.noreply.github.com>
2022-12-08 17:22:49 -0300

    DOM EN GARANTIA

--- JS/garantia.js
+++ JS/garantia.js
@@ -1,12 +1,12 @@
 // SECCION DE RECLAMOS
 class reclamo {
-    constructor(id, pedido, nombre, telefono, producto, marca){
+    constructor(id, pedido, nombre, telefono, producto, marca,falla){
         this.id = id,
         this.pedido = pedido,
         this.nombre = nombre,
         this.telefono = telefono,
         this.producto = producto,
-        this.marca = marca
+        this.marca = marca,
         this.falla = falla
 
     }
@@ -25,65 +25,68 @@ class reclamo {
 
 const reclamo1= new reclamo(1,111111,"Sarah Ochoa",1126950698,"luly","otra marca")
 const reclamo2= new reclamo(2,111111,"Isa Molina",1126950698,"joe 3","otra marca")
-const registroGarantia = [reclamo1,reclamo2]
 
-/*
-function garantia(){
-    let fecha1 = new Date(prompt("ingrese la fecha del dia en formato mm/dd/aa")) //fecha del dia
-    let fecha2 = new Date(prompt("ingrese la fecha en que recibió el producto o lo compró presencial en formato mm/dd/aa")) //fecha nueva
-    let milisegundosMes= 4.345*7*24*60*60*1000
-    let milisegundosTranscurridos = Math.abs(fecha1.getTime()- fecha2.getTime())
-    let mesesTranscurridos = Math.round(milisegundosTranscurridos/milisegundosMes)
-    console.log(`la cantidad de meses transcurridos desde la compra es: ${mesesTranscurridos}`)
+let registroGarantia = []
+if(localStorage.getItem("registroGarantia")){
+    registroGarantia = JSON.parse(localStorage.getItem(registroGarantia)) 
+}else{
+    console.log("seteando el array por primera vez")
+    registroGarantia.push(reclamo1,reclamo2)
+    localStorage.setItem("registroGarantia", JSON.stringify("registroGarantia"))
+}   
 
+console.log(registroGarantia)
 
-    function nuevoReclamo(array){
-        if( mesesTranscurridos <= 6){
-        let numeroPedido = parseInt(prompt("Ingrese el número de pedido: "))
-        let nombreIngresado = prompt ("Ingrese el nombre del cliente: ")
-        let numeroTelefono = parseInt(prompt("Ingrese el número de telefono del cliente: "))
-        let nombreProducto = prompt(" Ingrese el nombre del producto: ") 
-        let nombreMarca = prompt(" Ingrese el nombre de la marca: ")
-        let reclamoCreado = new reclamo (array.length+1,numeroPedido,nombreIngresado,numeroTelefono,nombreProducto,nombreMarca)
-        array.push(reclamoCreado)
-        console.log(array)
-    }else{
-        alert("El producto se encuentra fuera del tiempo de garantía")
-    } }
-    nuevoReclamo(registroGarantia)
-     }
-     console.log(registroGarantia)
-
-
-function verGarantias(array){
-    for(let reclamo of array){
-        console.log (`DATOS DE LA SOLICITUD DE GARANTIA:
-        Reclamo:  ${reclamo.id}
-        Pedido:   ${reclamo.pedido}
-        Cliente:  ${reclamo.nombre}
-        Teléfono: ${reclamo.telefono}
-        Producto: ${reclamo.producto}
-        Marca:    ${reclamo.marca} `)
-    }
-}
-
-
-
-function buscarPorReclamo (array){
-    let reclamoBuscado = parseInt(prompt("Ingrese el numero de reclamo que desea buscar"))
-    let reclamoEncontrado = array.find(
-        (reclamo)=>reclamo.id == reclamoBuscado
-    )
-    if (reclamoEncontrado == undefined){
-        console.log(`El reclamo numero ${reclamoBuscado} no se encuentra en el registro de garantías`)
-    }else{
-        console.log(reclamoEncontrado)
-    }
-}
-*/
-
-//DOM 
-let divGarantias = document.getElementById ("garantias")
+//DOM
+let divReclamos = document.getElementById ("reclamos")
 let btnGuardarGarantia = document.getElementById("guardarGarantiaBtn")
 let buscador = document.getElementById("buscador")
 let btnVerCatalogo = document.getElementById("verGarantias")
+
+function verGarantias(array){
+
+   divReclamos.innerHTML = ""
+    for (let reclamo of array){
+        let nuevoReclamo = document.createElement ("div")
+        nuevoReclamo.innerHTML = ` 
+        <div  class="card text-white bg-danger mb-3" style="max-width: 18rem;">
+          <div id="${reclamo.id} class="card-header">Garantia</div>
+          <div class="card-body">
+            <h5 class="card-title">${reclamo.pedido} </h5>
+            <p class="card-text">  ${reclamo.nombre}</p>
+            <p class="card-text">  ${reclamo.telefono}</p>
+            <p class="card-text">  ${reclamo.producto}</p>
+            <p class="card-text">  ${reclamo.marca}</p>
+            <p class="card-text">  ${reclamo.falla}</p>
+            <button type="button" class="btn btn-secondary">Eliminar</button>
+            <button type="button" class="btn btn-light">Editar</button>
+          </div>
+        </div>>`
+        divReclamos.appendChild(nuevoReclamo)
+
+    }
+
+}
+//funcion para agregar libros :
+function cargarReclamo(array) {
+    let inputPedido =  document.getElementById("pedidoInput")
+    let inputNombre = document.getElementById("nombreInput")
+    let inputTelefono = document.getElementById("telefonoInput")
+    let inputProducto = document.getElementById("productoInput")
+    let inputMarca = document.getElementById("marcaInput")
+    let inputFalla = document.getElementById("fallaInput")
+    let reclamoCreado = new  reclamo (array.length+1, inputPedido.value,inputNombre.value,inputTelefono.value,inputProducto.value,inputMarca.value,inputFalla.value)
+    array.push(reclamoCreado)
+    localStorage.setItem("registroGarantia",JSON.stringify(array))
+    verGarantias(array)
+    console.log(array)
+    inputPedido.value = ""
+    inputNombre.value = ""
+    inputTelefono.value = ""
+    inputProducto.value = ""
+    inputMarca.value = ""
+    inputFalla.value = ""
+}
+//evento
+btnGuardarGarantia.addEventListener("click",()=>{cargarReclamo(registroGarantia)})
+verGarantias(registroGarantia)
--- JS/registrogarantia.js
+++ JS/registrogarantia.js
@@ -1,11 +1,11 @@
 class reclamo {
-    constructor(id, pedido, nombre, telefono, producto, marca){
+    constructor(id, pedido, nombre, telefono, producto, marca,falla){
         this.id = id,
         this.pedido = pedido,
         this.nombre = nombre,
         this.telefono = telefono,
         this.producto = producto,
-        this.marca = marca
+        this.marca = marca,
         this.falla = falla
 
     }
@@ -27,40 +27,31 @@ const reclamo2= new reclamo(2,111111,"Isa Molina",1126950698,"joe 3","otra marca
 const registroGarantia = [reclamo1,reclamo2]
 
 //DOM 
-let divGarantias = document.getElementById ("garantias")
+let divReclamos = document.getElementById ("reclamos")
 let btnGuardarGarantia = document.getElementById("guardarGarantiaBtn")
 let buscador = document.getElementById("buscador")
 let btnVerCatalogo = document.getElementById("verGarantias")
 
 function verGarantias(array){
 
-    /*
-    for(let reclamo of array){
-        console.log (`DATOS DE LA SOLICITUD DE GARANTIA:
-        Reclamo:  ${reclamo.id}
-        Pedido:   ${reclamo.pedido}
-        Cliente:  ${reclamo.nombre}
-        Teléfono: ${reclamo.telefono}
-        Producto: ${reclamo.producto}
-        Marca:    ${reclamo.marca}
-        Falla:    ${reclamo.falla}`)
-    }
-    */
-
-   divGarantias.innerHTML =  ""
+   divReclamos.innerHTML =  ""
     for (let reclamo of array){
         let nuevoReclamo = document.createElement ("div")
-        nuevoReclamo.innerHTML = `  <div id="$garantias"></div>
-        <div class="card text-white bg-danger mb-3" style="max-width: 18rem;">
+        nuevoReclamo.innerHTML = ` 
+        <div id="${reclamo.id} class="card text-white bg-danger mb-3" style="max-width: 18rem;">
           <div class="card-header">Garantia</div>
           <div class="card-body">
-            <h5 class="card-title">Pedido</h5>
-            <p class="card-text">datos 
-            </p>
+            <h5 class="card-title">${reclamo.pedido} </h5>
+            <p class="card-text">  ${reclamo.nombre}</p>
+            <p class="card-text">  ${reclamo.telefono}</p>
+            <p class="card-text">  ${reclamo.producto}</p>
+            <p class="card-text">  ${reclamo.marca}</p>
+            <p class="card-text">  ${reclamo.falla}</p>
             <button type="button" class="btn btn-secondary">Eliminar</button>
             <button type="button" class="btn btn-light">Editar</button>
           </div>
-        </div>>`
-    }
+        </div>`
+        divReclamos.appendChild(nuevoReclamo)
 
+    }
 }
