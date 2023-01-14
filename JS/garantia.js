3b7e6c78d39cf53ebb6d9e74ea18eb3bbebd31ed
Maria Isabel Molina <101520961+isasoul@users.noreply.github.com>
2023-01-14 20:51:18 -0300

    agrego cambios

--- JS/garantia.js
+++ JS/garantia.js
@@ -1,17 +1,17 @@
 // SECCION DE RECLAMOS
 class reclamo {
-    constructor(id, pedido, nombre, telefono, producto, marca,falla){
+    constructor(id, pedido, nombre, telefono, producto, marca, falla) {
         this.id = id,
-        this.pedido = pedido,
-        this.nombre = nombre,
-        this.telefono = telefono,
-        this.producto = producto,
-        this.marca = marca,
-        this.falla = falla
+            this.pedido = pedido,
+            this.nombre = nombre,
+            this.telefono = telefono,
+            this.producto = producto,
+            this.marca = marca,
+            this.falla = falla
 
     }
 
-    mostrardatos(){
+    mostrardatos() {
         console.log(`Datos de la Solicitud:
         NUMERO DE RECLAMO:    ${this.id}
         NUMERO DE PEDIDO:     ${this.pedido}
@@ -19,48 +19,54 @@ class reclamo {
         NUMERO DE TELEFONO:   ${this.telefono}
         NOMBRE DEL PRODUCTO:  ${this.producto}
         NOMBRE DE LA MARCA:   ${this.marca}
-        NOMBRE DE LA FALLA:   ${this.falla}` )
+        NOMBRE DE LA FALLA:   ${this.falla}`)
     }
 }
 console.log(reclamo)
 
-const reclamo1= new reclamo(1,111111,"Sarah Ochoa",1126950698,"luly","otra marca","no enciende")
-const reclamo2= new reclamo(2,111111,"Isa Molina",1126950698,"joe 3","otra marca","no enciende")
+const reclamo1 = new reclamo(1, 111111, "Sarah Ochoa", 1126950698, "luly", "otra marca", "no enciende")
+const reclamo2 = new reclamo(2, 111111, "Isa Molina", 1126950698, "joe 3", "otra marca", "no enciende")
 
 
-let registroGarantia = [reclamo1,reclamo2]
+let registroGarantia = [reclamo1, reclamo2]
 
 console.log(registroGarantia)
 
 //DOM
 let form = document.getElementById("form")
 
-let divReclamos = document.getElementById ("reclamos")
+let divReclamos = document.getElementById("reclamos")
 let btnGuardarGarantia = document.getElementById("guardarGarantiaBtn")
 let buscador = document.getElementById("buscador")
 let btnVerCatalogo = document.getElementById("verGarantias")
 
 
-function verGarantias(array){
+function verGarantias(array) {
 
-   divReclamos.innerHTML = ""
-    for (let reclamo of array){ 
-        let nuevoReclamo = document.createElement ("div") 
+    divReclamos.innerHTML = ""
+    for (let reclamo of array) {
+        let nuevoReclamo = document.createElement("div")
         nuevoReclamo.innerHTML = ` 
-       <span class= "card__garantia"><div  class=" card text-white bg-danger mb-3" style="max-width: 18rem;">
-       <div id="${reclamo.id} class="card-header">Garantia ${reclamo.id}</div>
-       <div class="card-body">
-         <h5 class="card-title">${reclamo.pedido} </h5>
-         <p class="card-text">  ${reclamo.nombre}</p>
-         <p class="card-text">  ${reclamo.telefono}</p>
-         <p class="card-text">  ${reclamo.producto}</p>
-         <p class="card-text">  ${reclamo.marca}</p>
-         <p class="card-text">  ${reclamo.falla}</p>
-         <button type="button" class="btn btn-secondary">Eliminar</button>
-         <button type="button" class="btn btn-light">Editar</button>
-       </div>
-     </div>>
-     </span> 
+       >
+                <div class="row">        
+                    <span class= "card__garantia"><div  class=" card text-white bg-danger mb-3" style="max-width: 18rem;">
+                        <div id="${reclamo.id} class="card-header">Garantia ${reclamo.id}</div>
+                            <div class="card-body">
+                                <h5 class="card-title">${reclamo.pedido} </h5>
+                                <p class="card-text">  ${reclamo.nombre}</p>
+                                <p class="card-text">  ${reclamo.telefono}</p>
+                                <p class="card-text">  ${reclamo.producto}</p>
+                                <p class="card-text">  ${reclamo.marca}</p>
+                                <p class="card-text">  ${reclamo.falla}</p>
+                                <button type="button" class="btn btn-secondary">Eliminar</button>
+                                <button type="button" class="btn btn-light">Editar</button>
+                            </div>
+                        </div>>
+                    </span>  
+              
+                </div>
+        
+       
 
       `
         divReclamos.appendChild(nuevoReclamo)
@@ -70,17 +76,17 @@ function verGarantias(array){
 }
 
 function cargarReclamo(array) {
-    let inputPedido =  document.getElementById("pedidoInput").value
+    let inputPedido = document.getElementById("pedidoInput").value
     console.log(inputPedido)
     let inputNombre = document.getElementById("clienteInput").value
     let inputTelefono = document.getElementById("telefonoInput").value
     let inputProducto = document.getElementById("productoInput").value
     let inputMarca = document.getElementById("marcaInput").value
     let inputFalla = document.getElementById("fallaInput").value
-    let reclamoCreado = new  reclamo (array.length+1, inputPedido,inputNombre,inputTelefono,inputProducto,inputMarca,inputFalla)
+    let reclamoCreado = new reclamo(array.length + 1, inputPedido, inputNombre, inputTelefono, inputProducto, inputMarca, inputFalla)
     console.log(reclamoCreado)
     array.push(reclamoCreado)
-    localStorage.setItem("registroGarantia",JSON.stringify(array))
+    localStorage.setItem("registroGarantia", JSON.stringify(array))
     verGarantias(array)
     console.log(array)
     inputPedido.value = ""
@@ -91,25 +97,25 @@ function cargarReclamo(array) {
     inputFalla.value = ""
 }
 //evento
-form.addEventListener("submit",(e) => {
+form.addEventListener("submit", (e) => {
     e.preventDefault()
     cargarReclamo(registroGarantia)
 })
 
 
 let btnVerificarFecha = document.getElementById("verificarFechaBtn")
-btnVerificarFecha.addEventListener("click",(verificarFechaGarantia) )
-function verificarFechaGarantia(){
+btnVerificarFecha.addEventListener("click", (verificarFechaGarantia))
+function verificarFechaGarantia() {
     let fechaCompra = new Date(document.getElementById("fechaCompra").value);
     let fechaActual = new Date(document.getElementById("fechaActual").value);
-    let milisegundosMes= 4.345*7*24*60*60*1000
-    let milisegundosTranscurridos = Math.abs(fechaCompra.getTime()- fechaActual.getTime())
-    let mesesTranscurridos = Math.round(milisegundosTranscurridos/milisegundosMes)
+    let milisegundosMes = 4.345 * 7 * 24 * 60 * 60 * 1000
+    let milisegundosTranscurridos = Math.abs(fechaCompra.getTime() - fechaActual.getTime())
+    let mesesTranscurridos = Math.round(milisegundosTranscurridos / milisegundosMes)
     console.log(`la cantidad de meses transcurridos desde la compra es: ${mesesTranscurridos}`)
     alert(`la cantidad de meses transcurridos desde la compra es: ${mesesTranscurridos}`)
-    if(mesesTranscurridos <= 6 ){
+    if (mesesTranscurridos <= 6) {
         alert("El Producto esta dentro del tiempo de garantía")
-    }else{
+    } else {
         alert("EL PRODUCTO SE ENCUENTRA FUERA DEL TIEMPO DE GARANTIA")
     }
     fechaCompra.value = ""
